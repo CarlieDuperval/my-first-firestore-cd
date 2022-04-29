856f91e8a8894e1babf16e5de59947bae3c1d8c1
Carlie Duperval <carlierichell@gmail.com>
2022-04-29 11:16:54 -0400

    created first restaurant

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+const serviceAccount = require('./credentials.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+//connection to firestore project
+const db = admin.firestore();
+
+const restaurant ={
+    name:'Mister O1',
+    address: '555 fededral hwy suite 5',
+    cuisine:'Pizza',
+    rating:4.9,
+    phone:'(786) 677-2903',
+
+}
+// add the restaurant objetc in the firebase
+ db.collection('restaurants').add(restaurant)
+ .then( doc => console.log('Created restaurant', doc.id))
+ .catch(err => console.error(err))
