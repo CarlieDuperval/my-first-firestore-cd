8faf64eb6770aab86c7b143a395c3a549ca17f48
Carlie Duperval <carlierichell@gmail.com>
2022-04-29 11:33:52 -0400

    Adding data to ways

--- index.js
+++ index.js
@@ -15,9 +15,26 @@ const restaurant ={
     cuisine:'Pizza',
     rating:4.9,
     phone:'(786) 677-2903',
-
 }
 // add the restaurant objetc in the firebase
- db.collection('restaurants').add(restaurant)
- .then( doc => console.log('Created restaurant', doc.id))
- .catch(err => console.error(err))
+//  db.collection('restaurants').add(restaurant)   // db = adm.firestore => go to firestore admin and create a collection named ''
+//  .then( doc => console.log('Created restaurant', doc.id))
+//  .catch(err => console.error(err))
+
+const restaurant2 = {
+    name:'Bolay',
+    address: '7660 W Palmetto',
+    cuisine: 'American',
+    rating: 4.6 ,
+};
+
+async function addReataurant(data) {
+    try {
+    const doc = await db.collection('restaurants').add(data)
+    console.log('Created restaurant', doc.id)
+    }catch (err){
+        console.error(err)
+
+    }
+} 
+addReataurant(restaurant2)
